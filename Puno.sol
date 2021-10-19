c1cf71aaed456c5c8eab698e62c52c184c19610e
akku2209 <48758028+akku2209@users.noreply.github.com>
2021-10-19 17:55:56 +0530

    Update Puno.sol

--- Puno.sol
+++ Puno.sol
@@ -3,14 +3,27 @@
 pragma solidity ^0.8.0;
 
 
-import "./BEP20.sol";
+import "./ERC20.sol";
 import "./Ownable.sol";
 
 
-contract Puno is BEP20, Ownable{
-    uint256  _totalSupply = 5100000 * 10 ** 18;
+contract Puno is ERC20, Ownable{
+    
+    uint256  _totalSupply        = 5100000 * 10 ** 18;
+    uint256  _rewardPercent      = 250000 * 10 ** 18;
+    uint256  _holdPercent        = 2799000 * 10 ** 18;
+    uint256  _maintenacePercent  = 51000 * 10 ** 18;
+    uint256  _circulatingPercent = 2000000 * 10 ** 18;
+    
+    address rewardWallet       = 0x4A250668b0610cA1aff58732346982F7d75120C1;
+    address maintenanceWallet  = 0x4A250668b0610cA1aff58732346982F7d75120C1;
+    address holdWallet         = 0x4A250668b0610cA1aff58732346982F7d75120C1;
+    
     constructor (string memory name, string memory symbol) BEP20(name, symbol) {
-        _mint(msg.sender, _totalSupply);
+        _mint(msg.sender, _circulatingPercent);
+        _mint(rewardWallet, _rewardPercent);
+        _mint(maintenanceWallet, _maintenacePercent);
+        _mint(holdWallet, _holdPercent);
     }
     
     function burn(uint256 amount) external  {
@@ -19,6 +32,18 @@ contract Puno is BEP20, Ownable{
     
     function totalSupply() public view virtual override returns (uint256) {
         return _totalSupply;
+    }
+    
+    function setRewardWallet(address account) public onlyOwner {
+        rewardWallet = account;
+    }
+    
+    function setMainetanceWallet(address account) public onlyOwner {
+        maintenanceWallet = account;
+    }
+    
+    function setHoldWallet(address account) public onlyOwner {
+        holdWallet = account;
     }
     
 }
