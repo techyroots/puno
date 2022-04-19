cd2d68903262fa7f2bb5d7c6e45962bce63faef0
akku2209 <48758028+akku2209@users.noreply.github.com>
2022-04-19 10:05:24 +0530

    Add files via upload

--- ICO.sol
+++ ICO.sol
@@ -25,6 +25,7 @@ contract ICO is Ownable, Pausable {
     // the number of tokens already sold through this contract
     uint256 private tokensSold = 0;
     
+    // timestamp of 6 months.
     uint256 private lockTime  = 15724800;
     
     struct User{
@@ -39,15 +40,17 @@ contract ICO is Ownable, Pausable {
         uint256 buyTime;
     }
 
+    // How much tokens each address has withdarw
     mapping(address => uint256) private withdraw;
 
     // How much BNB each address has invested to this crowdsale
     mapping(address => User) private userDetails;
     
+    // Each address contain number of investments.
     mapping (address => mapping (uint => Buy)) private buyDetails;
     
     // A new investment was made
-    event Invested(address indexed investor, uint256 investAmount, uint256 tokenAmount);
+    event Invested(address indexed investor, uint256 investAmount, uint256 tokenAmount, uint256 time);
     
     // Crowdsale Start time has been changed
     event StartsAtChanged(uint256 startsAt);
@@ -62,42 +65,50 @@ contract ICO is Ownable, Pausable {
     event RateChanged(uint256 oldValue, uint256 newValue);
     
     // Withdraw Event
-    event Withdraw(uint256 amount, address indexed user, uint256 withdrawTime);
+    event Withdraw(uint256 amount, address indexed user, address indexed to, uint256 withdrawTime);
     
+    // Intialize the token contract address.
     function initialize(address _token) public onlyOwner{
         require(_token != address(0), "Invalid Address");
         token = ITRC20(_token);
     }
     
+    // Pause the sale
     function pause() public onlyOwner {
         _pause();
     }
 
+    // Unpause the sale
     function unpause() public onlyOwner {
         _unpause();
     } 
     
+    // Set the sale start time
     function setStartsAt(uint256 time) public onlyOwner{
         startsAt = time;
         emit StartsAtChanged(startsAt);
     }
     
+    // Set the sale end time
     function setEndsAt(uint256 time) public onlyOwner {
         endsAt = time;
         emit EndsAtChanged(endsAt);
     }
     
+    // Set the token price
     function setRate(uint256 value) public onlyOwner{
         require(value > 0);
         tokenPrice = value;
         emit RateChanged(tokenPrice, value);
     }
     
+    // Change the lock time
     function setLockTime(uint256 time) public onlyOwner{
         lockTime = time;
         emit LockTimeChanged(time);
     }
     
+    // Invest in crowdsale
     function invest() public payable whenNotPaused{ 
         require(startsAt <= block.timestamp && endsAt > block.timestamp);
         uint256 tokensAmount = (msg.value).mul(10**6).div(tokenPrice);  // check
@@ -123,7 +134,7 @@ contract ICO is Ownable, Pausable {
          // Transfer Fund to owner's address
         payable(owner()).transfer(address(this).balance);
         // Emit an event that shows invested successfully
-        emit Invested(msg.sender, msg.value, tokensAmount);
+        emit Invested(msg.sender, msg.value, tokensAmount, block.timestamp);
     }
 
     function transferTokens(uint256 amount) public onlyOwner{
@@ -131,19 +142,42 @@ contract ICO is Ownable, Pausable {
         token.transfer(owner(), amount);
     } 
     
-    function withdrawal(uint256 amount) public {
+    function withdrawal(address to, uint256 amount) public {
         uint256 releaseAmount = 0;
         require(amount > 0, "Amount Not be zero");
         for(uint i = 1 ; i <= userDetails[msg.sender].totalBuy ; i++){
             if(block.timestamp > buyDetails[msg.sender][i].buyTime + lockTime){
-              uint256 time = (block.timestamp - (buyDetails[msg.sender][i].buyTime + lockTime)).div(30 days);
-              releaseAmount = releaseAmount + (buyDetails[msg.sender][i].tokenAmount).div(5).mul(time);
+              uint256 time = (block.timestamp - (buyDetails[msg.sender][i].buyTime + lockTime)).div(300);
+              if(time == 1){
+                releaseAmount = releaseAmount + (buyDetails[msg.sender][i].tokenAmount).mul(30).div(100);
+              }else if(time == 2){
+                releaseAmount = releaseAmount + (buyDetails[msg.sender][i].tokenAmount).mul(60).div(100);
+              }else if(time > 2){
+                releaseAmount = releaseAmount + (buyDetails[msg.sender][i].tokenAmount);
+              }
             }
         }
         require(releaseAmount - userDetails[msg.sender].withdrawAmount >= amount, "Not Enough Amount");
-        token.transfer(msg.sender, amount);
+        token.transfer(to, amount);
         userDetails[msg.sender].withdrawAmount += amount;
-        emit Withdraw(amount, msg.sender, block.timestamp);
+        emit Withdraw(amount, msg.sender, to, block.timestamp);
+    }
+
+    function viewReleaseAmount(address to) public view returns (uint) {
+        uint256 releaseAmount = 0;
+        for(uint i = 1 ; i <= userDetails[to].totalBuy ; i++){
+            if(block.timestamp > buyDetails[to][i].buyTime + lockTime){
+              uint256 time = (block.timestamp - (buyDetails[to][i].buyTime + lockTime)).div(300);
+              if(time == 1){
+                releaseAmount = releaseAmount + (buyDetails[to][i].tokenAmount).mul(30).div(100);
+              }else if(time == 2){
+                releaseAmount = releaseAmount + (buyDetails[to][i].tokenAmount).mul(60).div(100);
+              }else if(time > 2){
+                releaseAmount = releaseAmount + (buyDetails[to][i].tokenAmount);
+              }
+            }
+        }
+        return (releaseAmount - userDetails[to].withdrawAmount);
     }
     
     function price() public view returns (uint256){
